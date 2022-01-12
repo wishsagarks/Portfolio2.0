004eb2ec5895f8c10c6b79a904cbfcbaf7b63d2d
wishsagarks <wishsagarks@gmail.com>
2022-01-12 20:10:32 +0530

    mail php added

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "wishsagarks@gmail.com";
+$sitename = "wish";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
